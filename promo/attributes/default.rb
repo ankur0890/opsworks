d6138cb3f5de07e38fd18bde0e529dd15a1845d2
Eric Ruvio <eruvio90@gmail.com>
2014-06-11 09:21:53 -0400

    updated application ini erb

---
+++ promo/attributes/default.rb
@@ -0,0 +1,15 @@
+default[:promo] = {}
+
+default[:promo][:mysql] = {}
+default[:promo][:mysql][:host] = "hebsmysqlproduction.cwpwch6sm7qc.us-east-1.rds.amazonaws.com"
+default[:promo][:mysql][:username] = ""
+default[:promo][:mysql][:password] = ""
+default[:promo][:mysql][:database] = ""
+default[:promo][:mysql][:aws_accessKey] = ""
+default[:promo][:mysql][:aws_secretKey] = ""
+default[:promo][:mysql][:aws_s3_bucket] = ""
+default[:promo][:mysql][:aws_s3_bucket_prefix] = ""
+default[:promo][:mysql][:aws_ses_accessKey] = ""
+default[:promo][:mysql][:aws_ses_secretKey] = ""
+
+default[:promo][:nginx][:customConfig] = ""
